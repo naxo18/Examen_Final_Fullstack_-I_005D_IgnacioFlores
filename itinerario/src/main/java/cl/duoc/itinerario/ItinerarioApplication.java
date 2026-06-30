e966246b32502507f8a436407abea345bd36ccf4
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 11:26:50 -0400

    subir proyecto base con Eureka, Gateway, Login, Destination y Traveler

---
+++ itinerario/src/main/java/cl/duoc/itinerario/ItinerarioApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.itinerario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItinerarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItinerarioApplication.class, args);
+	}
+
+}
